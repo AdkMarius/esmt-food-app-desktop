3f22d175f71ec75f40cc7bcca36bb07aa38fcd57
Cornelliah <horianehkr@yahoo.com>
2024-07-30 23:58:17 +0000

    afficher le contenu de app.jsx

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/renderer.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'src'),
     filename: 'bundle.js'
   },
   module: {
