193c007594bbc3f8df9557736a1cf86f32e6e635
Horiane Cornellia <101379780+Cornelliah@users.noreply.github.com>
2024-08-01 09:24:36 +0000

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,1 +1,13 @@
 const path= require('path');
+
+const {contextBridge, ipcRenderer }= require ('electron');
+
+contextBridge.exposeInMainWorld ('path', {
+    join: (...args)=> path.join(...args)
+});
+
+contextBridge.exposeInMainWorld ('ipcRenderer', {
+    send: (channel, data)=> ipcRenderer.send(channel,data),
+    on:(channel, func)=>
+        ipcRenderer.on (channel, (event, ...args)=> func (...args))
+});
