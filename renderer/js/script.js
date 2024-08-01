289edb1c534bb26ad0597a85e1b112390d32a63b
MohamedSadio <159494473+MohamedSadio@users.noreply.github.com>
2024-08-01 10:52:56 +0000

    nav

--- renderer/js/script.js
+++ renderer/js/script.js
@@ -2,3 +2,16 @@
     function closeForm() {
       ipcRenderer.send('close-main-window');
     }
+
+    // Assurez-vous que le script est exécuté après le chargement du DOM
+document.addEventListener('DOMContentLoaded', function() {
+    // Ciblez le formulaire ou le bouton de soumission
+    const loginForm = document.getElementById('login-form');
+    
+    // Ajoutez un gestionnaire d'événements pour empêcher l'envoi du formulaire et rediriger l'utilisateur
+    loginForm.addEventListener('submit', function(event) {
+      event.preventDefault(); // Empêche le comportement de soumission par défaut
+      window.location.href = 'produits.html'; // Redirige vers produits.html
+    });
+  });
+  
