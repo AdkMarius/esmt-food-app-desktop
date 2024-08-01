4867ec9c8fe5e3a4ff6e012b2866a7f217873525
Cornelliah <horianehkr@yahoo.com>
2024-08-01 10:15:22 +0000

    ipc main

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require ('path');
 const isMac = process.platform === 'darwin';
 const isDev= process.env.NODE_ENV !== 'production';
