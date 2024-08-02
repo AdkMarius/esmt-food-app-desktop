2e1fa5df4b5e1906ec3cbe5011d884c5ba4cd05b
Cornelliah <horianehkr@yahoo.com>
2024-08-02 12:54:20 +0000

    requêtes

--- renderer/js/script.js
+++ renderer/js/script.js
@@ -3,70 +3,87 @@ function closeForm() {
     ipcRenderer.send('close-main-window');
 }
 
-    document.addEventListener('DOMContentLoaded', function() {
-        const loginForm = document.getElementById('login-form');
-      
-        loginForm.addEventListener('submit', function(event) {
-          event.preventDefault(); 
-      
-          const username = document.getElementById('username').value;
-          const password = document.getElementById('password').value;
-      
-          // Charger les utilisateurs depuis le fichier JSON
-          fetch('data/users.json')
-            .then(response => response.json())
-            .then(users => {
-              // Vérifier les informations d'identification
-              const user = users.find(u => u.username === username && u.password === password);
-      
-              if (user) {
-                // Authentification réussie, redirection vers produits.html
-                window.location.href = 'produits.html';
-              } else {
-                // Authentification échouée, afficher un message d'erreur
-                alert('Nom d’utilisateur ou mot de passe incorrect');
-              }
-            })
-            .catch(error => console.error('Erreur lors du chargement des utilisateurs:', error));
-        });
-      });
-      
-  
+//Authentification
+document.addEventListener('DOMContentLoaded', function() {
+  const loginForm = document.getElementById('login-form');
 
-  function loadOrders() {
-    fetch('data/orders.json')
-        .then(response => response.json())
-        .then(data => {
-            const tableBody = document.getElementById('orders-table-body');
-            data.forEach(order => {
-                const row = document.createElement('tr');
-                row.innerHTML = `
-                    <td>${order.reference}</td>
-                    <td>${order.date}</td>
-                    <td>${order.price_total.toFixed(2)} F</td>
-                `;
-                tableBody.appendChild(row);
-            });
-        })
-        .catch(error => console.error('Erreur lors du chargement des données:', error));
+  loginForm.addEventListener('submit', async function(event) {
+      event.preventDefault();
+
+      const email = document.getElementById('username').value;
+      const password = document.getElementById('password').value;
+
+      try {
+          const response = await fetch('http://localhost:3000/auth/sign-in', {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({ email, password })
+          });
+
+          const result = await response.json();
+
+          if (response.ok) {
+              // Authentification réussie, redirection vers produits.html
+              window.location.href = 'produits.html';
+          } else {
+              // Authentification échouée, afficher un message d'erreur
+              alert(result.message);
+          }
+      } catch (error) {
+          console.error('Erreur lors de la connexion:', error);
+          alert('Erreur lors de la connexion. Veuillez réessayer plus tard.');
+      }
+  });
+});
+
+  
+//Historique
+async function loadOrders() {
+  try {
+      const response = await fetch('http://localhost:3000/api/orders/list');
+      const data = await response.json();
+
+      if (!response.ok) {
+          throw new Error(data.message || 'Failed to fetch orders');
+      }
+
+      const tableBody = document.getElementById('orders-table-body');
+      tableBody.innerHTML = ''; 
+
+      data.data.forEach(order => {
+          const row = document.createElement('tr');
+          row.innerHTML = `
+              <td>${order.id}</td>
+              <td>${order.created_at}</td>
+              <td>${order.total_price.toFixed(2)} F</td>
+          `;
+          tableBody.appendChild(row);
+      });
+  } catch (error) {
+      console.error('Erreur lors du chargement des données:', error);
+  }
 }
+
 
 
 document.addEventListener('DOMContentLoaded', loadOrders);
 
 
+//Produits liste, ajout et mise à jour
 
 document.addEventListener("DOMContentLoaded", function() {
   // Fetch the products data
-  fetch('data/produits.json')
+  fetch('/api/products/list')
       .then(response => response.json())
       .then(products => {
           const productList = document.getElementById('product-list');
-          products.forEach(product => {
+          products.data.forEach(product => {
               // Create card element
               const card = document.createElement('div');
               card.className = 'card';
-              
+
               if (product.image) {
                   const img = document.createElement('img');
                   img.className = 'card-img-top';
@@ -97,22 +114,51 @@ document.addEventListener("DOMContentLoaded", function() {
               details.appendChild(price);
 
               const viewIcon = document.createElement('img');
-              viewIcon.className = 'img-fluid'
+              viewIcon.className = 'img-fluid';
               viewIcon.src = 'image/voir.png';
               viewIcon.alt = 'Voir';
               viewIcon.style.cursor = 'pointer';
               details.appendChild(viewIcon);
 
               viewIcon.addEventListener('click', () => {
-                document.getElementById('productName').value = product.name;
-                document.getElementById('productPrice').value = product.price;
-                document.getElementById('productId').value = product.id;
-                $('#editProductModal').modal('show');
-            });
+                  document.getElementById('productName').value = product.name;
+                  document.getElementById('productPrice').value = product.price;
+                  document.getElementById('productId').value = product.id;
+                  $('#editProductModal').modal('show');
+              });
 
               productList.appendChild(card);
-              
           });
       })
       .catch(error => console.error('Error loading products:', error));
+
+  // Save changes event
+  document.getElementById('saveProductChanges').addEventListener('click', () => {
+      const productId = document.getElementById('productId').value;
+      const updatedProduct = {
+          name: document.getElementById('productName').value,
+          price: document.getElementById('productPrice').value
+      };
+
+      fetch(`/api/products/${productId}`, {
+          method: 'PUT',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(updatedProduct)
+      })
+      .then(response => response.json())
+      .then(data => {
+          if (data.message === 'Product read successfully') {
+              alert('Product updated successfully');
+              $('#editProductModal').modal('hide');
+              // Reload the products or update the DOM accordingly
+              // For simplicity, reloading the page is an option
+              location.reload();
+          } else {
+              alert('Error updating product');
+          }
+      })
+      .catch(error => console.error('Error updating product:', error));
+  });
 });
